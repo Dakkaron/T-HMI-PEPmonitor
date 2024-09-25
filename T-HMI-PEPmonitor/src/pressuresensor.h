3f757a145a9bf0f673eaa8cb470aa55c5e71f469
Dakkaron <dak1st@gmx.at>
2024-09-25 22:05:46 +0200

    Move from Arduino IDE to Platformio

---
+++ T-HMI-PEPmonitor/src/pressuresensor.h
@@ -0,0 +1,11 @@
+#ifndef __PRESSURESENSOR_H__
+#define __PRESSURESENSOR_H__
+
+#include <Arduino.h>
+#include "config.h"
+#include <Adafruit_HX711.h>
+#include "constants.h"
+
+void readPressure(Adafruit_HX711* hx711, BlowData* blowData);
+
+#endif /*PRESSURESENSOR*/
